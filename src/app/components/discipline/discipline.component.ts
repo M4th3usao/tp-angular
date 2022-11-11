adf95a18592b45fc50919764fafb1ab804d4c9f9
Matheus Henrique <matheus.rico17@gmail.com>
2022-11-11 00:16:59 -0300

    disciplines

--- src/app/components/discipline/discipline.component.ts
+++ src/app/components/discipline/discipline.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Discipline } from '../Discipline';
+
+import { DisciplineService } from 'src/app/services/disciplines.service';
+
 @Component({
   selector: 'app-discipline',
   templateUrl: './discipline.component.html',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisciplineComponent implements OnInit {
 
-  constructor() { }
+  disciplines: Discipline[] = [];
+
+  constructor(private disciplineService: DisciplineService) { 
+    this.getDisciplines()
+  }
 
   ngOnInit(): void {
   }
 
+  
+  getDisciplines(): void {
+    this.disciplineService.getAll().subscribe((disciplines) => (this.disciplines = disciplines));
+  }
 }
---
+++ src/app/services/disciplines.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { Discipline } from "../components/Discipline";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DisciplineService {
+    private apiUrl = 'http://localhost:3000/disciplines'
+
+
+constructor(private http: HttpClient) { }
+
+
+getAll(): Observable<Discipline[]> {
+    return this.http.get<Discipline[]>(this.apiUrl)//isso é um padrão do angular, pois ele precisa observar se o arry chega na requisição da api.
+  }
+
+}
